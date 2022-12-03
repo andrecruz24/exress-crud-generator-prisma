cfbfb089b4c613496f755affa59f220ae093fc69
andrecruz24 <cruzandr8@gmail.com>
2022-12-03 13:04:24 +0000

    linting

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const crud_controller = {};
 
 function parseNumber(value) {
 	// Make sure it is a number
-	if (isNaN(valueToParse)) {
+	if (isNaN(value)) {
 		// If it's not a number return null
 		return null;
 	}
 	// Transform the id from a String to a Number
-	return Number(valueToParse);
+	return Number(value);
 }
 
 
@@ -17,7 +17,7 @@ crud_controller.getAll = (model) => async (req, res) => {
 	if (all_itens.length > 0)
 		return res.json(all_itens);
 	res.status(204);
-}
+};
 
 crud_controller.getById = (model) => async (req, res, next) => {
 	// Get the case id from the url params
@@ -28,20 +28,16 @@ crud_controller.getById = (model) => async (req, res, next) => {
 
 	if (!id) {
 		res.status(400);
-		return next(error);
+		return next("Id needs to be a number");
 	}
 
 	const requested_item= await model.findUnique({
 		where: { id },
-	})
+	});
 
-	// Try to get the item with the requested id, if it fails return an error
-	try {
-		const requested_item = await model.findUniqueOrThrow({
-			where: { id }
-		});
+	if (requested_item) {
 		res.json(requested_item);
-	} catch (error) {
-		next(error);
+	} else {
+		next("Record not found");
 	}
-}
+};
